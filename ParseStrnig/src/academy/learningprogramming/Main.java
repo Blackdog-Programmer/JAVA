75121782e7cc67cd3db4188f45600ea0493b6f1b
blackdogcode <blackdogcode@gmail.com>
2019-11-11 16:38:28 +0900

    Java ParseString Example

---
+++ ParseStrnig/src/academy/learningprogramming/Main.java
@@ -0,0 +1,18 @@
+package academy.learningprogramming;
+
+import jdk.swing.interop.SwingInterOpUtils;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String numberAsString = "2019";
+
+	    int stringToNumber = Integer.parseInt(numberAsString);
+        System.out.println("This Year " + stringToNumber);
+
+        numberAsString += 1;
+        stringToNumber += 1;
+        System.out.println(numberAsString);
+        System.out.println(stringToNumber);
+    }
+}
