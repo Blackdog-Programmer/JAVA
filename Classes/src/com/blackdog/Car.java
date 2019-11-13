63afd3c65e1aa19987bbeaeb6ce03566106566ef
blackdogcode <blackdogcode@gmail.com>
2019-11-13 16:39:57 +0900

    JAVA Class Demo

---
+++ Classes/src/com/blackdog/Car.java
@@ -0,0 +1,13 @@
+package com.blackdog;
+
+public class Car {
+    private String model;
+    private double price;
+
+    public void setModel(String newModel) {
+        this.model = newModel;
+    }
+    public String getModel() {
+        return this.model;
+    }
+}
---
+++ Classes/src/com/blackdog/Main.java
@@ -0,0 +1,8 @@
+package com.blackdog;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Car sonata = new Car();
+    }
+}
