47ec8e53dba49faffada4d5a920b2aa5edfc882b
blackdogcode <blackdogcode@gmail.com>
2019-11-22 23:59:14 +0900

    Equal Sum Checker

---
+++ 04_Java_Tutorial_Expression_Statments_CodeBlocks_Methods/EqualSumChecker/src/com/blackdog/EqualSumChecker.java
@@ -0,0 +1,8 @@
+package com.blackdog;
+
+public class EqualSumChecker {
+        public static boolean hasEqualSum(int p, int q, int r) {
+            if (p + q == r) return true;
+            else return false;
+        }
+}
