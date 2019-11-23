c46c841885d3a535b1d9042db30c1d39cc88bab0
Brian, Song <deilttantest@gmail.com>
2019-11-23 21:03:19 +0900

    Playing Cat

---
+++ 04_Java_Tutorial_Expression_Statments_CodeBlocks_Methods/PlayingCat/src/com/blackdog/PlayingCat.java
@@ -0,0 +1,14 @@
+package com.blackdog;
+
+public class PlayingCat {
+    public static boolean isCatPlaying(boolean isSummer, int temperature) {
+        if (isSummer) {
+            if (temperature >= 25 && temperature <= 45) return true;
+            else return false;
+        }
+        else {
+            if (temperature >= 25 && temperature <= 35) return true;
+            else return false;
+        }
+    }
+}
