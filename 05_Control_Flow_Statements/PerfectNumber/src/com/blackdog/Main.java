7d464ad3f69a455d6ca7ebfde96ed05449c89813
Brian, Song <blackdogcode@gmail.com>
2019-11-25 22:11:35 +0900

    Check Number is Perfect Number

---
+++ 05_Control_Flow_Statements/PerfectNumber/src/com/blackdog/Main.java
@@ -0,0 +1,11 @@
+package com.blackdog;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(PerfectNumber.isPerfectNumber((6)));
+        System.out.println(PerfectNumber.isPerfectNumber((3)));
+        System.out.println(PerfectNumber.isPerfectNumber((5)));
+        System.out.println(PerfectNumber.isPerfectNumber((1)));
+    }
+}
