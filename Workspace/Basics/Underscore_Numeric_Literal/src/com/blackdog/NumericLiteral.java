d346c6584a34568072a85607f4eadeb6e77b609a
blackdogcode <blackdogcode@gmail.com>
2020-02-29 12:34:49 +0900

    undersocre numeric literal in java demo

---
+++ Workspace/Basics/Underscore_Numeric_Literal/src/com/blackdog/NumericLiteral.java
@@ -0,0 +1,18 @@
+package com.blackdog;
+
+public class NumericLiteral {
+
+	public static void main(String[] args) throws java.lang.Exception {
+		int inum = 1_00_00_000;
+		System.out.println("inum:" + inum);
+
+		long lnum = 1_00_00_000;
+		System.out.println("lnum:" + lnum);
+
+		float fnum = 2.10_001F;
+		System.out.println("fnum:" + fnum);
+
+		double dnum = 2.10_12_001;
+		System.out.println("dnum:" + dnum);
+	}
+}
